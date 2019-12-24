effa9daa3703903138205638b5640e096f73a95a
mukesh choudhary <mukachoudhary@gmail.com>
2019-12-24 16:46:19 +0530

    Updates

---
+++ precache-manifest.0cf423aaa9f53a9010a2a689ed6b0afd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf24c06be1325af157aa",
+    "url": "/react-auth0/static/js/runtime~main.bf24c06b.js"
+  },
+  {
+    "revision": "4a2cf2636ed2c3370088",
+    "url": "/react-auth0/static/js/main.4a2cf263.chunk.js"
+  },
+  {
+    "revision": "bde647d46c8815a7c07a",
+    "url": "/react-auth0/static/js/1.bde647d4.chunk.js"
+  },
+  {
+    "revision": "4a2cf2636ed2c3370088",
+    "url": "/react-auth0/static/css/main.59bfec9d.chunk.css"
+  },
+  {
+    "revision": "0e5f90570e030323d9521d013b8d6621",
+    "url": "/react-auth0/index.html"
+  }
+];
